b22c9073927724d0baaa6966bb0f3e2e88e0557d
Jose Storopoli <jose@storopoli.io>
2023-04-11 06:30:08 -0300

    feat: initial config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 💤 LazyVim
+# 💤 [Neovim](https://neovim.org) config with [LazyVim](https://github.com/LazyVim/LazyVim)
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/plugins/treesitter-custom.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.ignore_install = { "help" }
+
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, {
+        "dockerfile",
+        "git_config",
+        "jsdoc",
+        "make",
+        "toml",
+        "vimdoc",
+        "julia",
+        "rust",
+        "go",
+        "typescript",
+        "tsx",
+        "python",
+      })
+    end
+  end,
+}
