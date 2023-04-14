47b657d574d071c36bd0562a693e85f177527c8d
Jose Storopoli <jose@storopoli.io>
2023-04-14 09:09:48 -0300

    feat: vim-slime

---
+++ lua/plugins/slime.lua
@@ -0,0 +1,8 @@
+return {
+  "jpalardy/vim-slime",
+  lazy = false,
+  config = function()
+    vim.g.slime_target = "tmux"
+    vim.g.slime_default_config = { socket_name = "default", target_pane = "{left-of}" }
+  end,
+}
