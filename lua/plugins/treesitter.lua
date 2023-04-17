7cba3ebd6438ece04551fc69f76d873352dd77a0
Jose Storopoli <jose@storopoli.io>
2023-04-17 09:41:40 -0300

    fix: LazyVim treesitter help->vimdoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      opts.ignore_install = { "help" }
-
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
           "bash",
