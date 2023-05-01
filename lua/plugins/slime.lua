95ca3d027e8815b5367ebe0af90dd605a666c960
Jose Storopoli <jose@storopoli.io>
2023-05-01 10:19:37 -0300

    fix: add slime kitty boilertemplate

--- lua/plugins/slime.lua
+++ lua/plugins/slime.lua
@@ -2,6 +2,7 @@ return {
   "jpalardy/vim-slime",
   lazy = false,
   config = function()
+    -- vim.g.slime_target = "kitty"
     vim.g.slime_target = "tmux"
     vim.g.slime_default_config = { socket_name = "default", target_pane = "{left-of}" }
   end,
