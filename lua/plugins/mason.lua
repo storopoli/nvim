ae41ac500ca35540a7818c0aa7f63641290a0e01
Jose Storopoli <jose@storopoli.io>
2023-05-21 07:39:04 -0300

    feat: add Mason as a starter option

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,22 +1,42 @@
 return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "stylua",
-      "luacheck",
-      "shellcheck",
-      "rustfmt",
-      "prettierd",
-      "shfmt",
-      "black",
-      "isort",
-      "flake8",
-      "ruff",
-      "markdownlint",
-      "proselint",
-      "write-good",
-      "alex",
-      "typst-lsp",
-    })
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "luacheck",
+        "shellcheck",
+        "rustfmt",
+        "prettierd",
+        "shfmt",
+        "black",
+        "isort",
+        "flake8",
+        "ruff",
+        "markdownlint",
+        "proselint",
+        "write-good",
+        "alex",
+        "typst-lsp",
+      })
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, dashboard)
+      -- alpha button table
+      local tbl = dashboard.section.buttons.val
+
+      -- Mason button
+      local mason_button = dashboard.button("m", " " .. " Mason", ":Mason <CR>")
+      mason_button.opts.hl = "AlphaButtons"
+      mason_button.opts.hl_shortcut = "AlphaShortcut"
+
+      -- Calculate the index of the second to last position
+      local idx = #tbl -- (#tbl gives the size of the table)
+
+      -- Insert the Mason button at the second to last position
+      table.insert(dashboard.section.buttons.val, idx, mason_button)
+    end,
+  },
 }
