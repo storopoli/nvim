03f77ca51c4a5b10eb47dfcf33e785528a91d01f
Jose Storopoli <jose@storopoli.io>
2023-05-23 07:37:13 -0300

    feat:

--- lua/plugins/nvim-cmp-latex.lua
+++ lua/plugins/nvim-cmp-latex.lua
@@ -1,21 +1,26 @@
 -- override nvim-cmp and add cmp-emoji and cmp-latex-symbols
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-emoji",
-    "kdheepak/cmp-latex-symbols",
-  },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
-    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-      {
-        name = "latex_symbols",
-        option = {
-          strategy = 1, -- julia
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "kdheepak/cmp-latex-symbols",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        {
+          name = "latex_symbols",
+          option = {
+            strategy = 1, -- julia
+          },
         },
-      },
-    }))
-  end,
+      }))
+    end,
+  },
+  {
+    "evesdropper/luasnip-latex-snippets.nvim",
+  },
 }
