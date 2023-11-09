8a619bc7c832e32865b78a427d5437c16f8fc4ea
Jose Storopoli <jose@storopoli.io>
2023-11-09 19:25:39 -0300

    fix(color): notify background color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -65,4 +65,12 @@ return {
       })
     end,
   },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+    end,
+  },
 }
