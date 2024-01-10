6ecad41cef9ee4e16b7626b8dde932298aa89ef2
Jose Storopoli <jose@storopoli.io>
2024-01-10 19:29:19 -0300

    feat(copilot): remove copilot

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,44 +11,9 @@ return {
       "hrsh7th/cmp-nvim-lua", -- nvim-cmp source for neovim Lua API
       "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
       "L3MON4D3/LuaSnip", -- Snippets plugin
-      "folke/neodev.nvim", -- Neovim development Lua utils
-      "petertriho/cmp-git", -- nvim-cmp source for git
       "barreiroleo/ltex_extra.nvim", -- ltex-ls extra stuff: codeactions and language
-      -- Copilot
-      {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        build = ":Copilot auth",
-        opts = {
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-          filetypes = {
-            markdown = true,
-          },
-        },
-        keys = {
-          {
-            "<leader>cp",
-            function()
-              if require("copilot.client").is_disabled() then
-                vim.cmd("Copilot enable")
-              else
-                vim.cmd("Copilot disable")
-              end
-            end,
-            desc = "Co[p]ilot Toggle",
-          },
-        },
-      },
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        config = true,
-      },
     },
     config = function()
-      -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-      require("neodev").setup()
       local lsp = require("lspconfig")
       -- Global mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -101,12 +66,6 @@ return {
       local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
       luasnip.config.setup({})
-      -- tab fix for copilot
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
       cmp.setup({
         completion = {
           completeopt = "menu,menuone,noinsert",
@@ -116,16 +75,6 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        enabled = function()
-          -- disable completion in comments
-          local context = require("cmp.config.context")
-          -- keep command mode completion enabled when cursor is in a comment
-          if vim.api.nvim_get_mode().mode == "c" then
-            return true
-          else
-            return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-          end
-        end,
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -140,39 +89,16 @@ return {
               end
             elseif luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-              if #cmp.get_entries() == 1 then
-                cmp.confirm({ select = true })
-              end
             else
               fallback()
             end
           end, { "i", "s" }),
-          -- Accept currently selected item.
-          -- Set `select` to `false` to only confirm explicitly selected items.
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          -- Don't mess with my <CR>
-          --[[
-                    ["<CR>"] = cmp.mapping({
-                        i = function(fallback)
-                            if cmp.visible() and cmp.get_active_entry() then
-                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-                            else
-                                fallback()
-                            end
-                        end,
-                        s = cmp.mapping.confirm({ select = true }),
-                        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-                    }),
-                    --]]
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
             else
               fallback()
             end
@@ -188,63 +114,13 @@ return {
           end, { "i", "s" }),
         }),
         sources = {
-          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
-          -- { name = "buffer" },
+          { name = "buffer" },
           { name = "path" },
         },
-        sorting = { -- copilot_cmp suggestion
-          priority_weight = 2,
-          comparators = {
-            require("copilot_cmp.comparators").prioritize,
-            -- Below is the default comparator list and order for nvim-cmp
-            cmp.config.compare.offset,
-            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.locality,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
       })
-      -- If you want insert `(` after select function or method item
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      -- Set configuration for specific filetype.
-      cmp.setup.filetype("gitcommit", {
-        sources = cmp.config.sources({
-          { name = "git" },
-          { name = "buffer" },
-          { name = "copilot" },
-        }),
-      })
-      cmp.setup.filetype({ "markdown", "text", "sql" }, {
-        sources = cmp.config.sources({
-          { name = "buffer" },
-          { name = "copilot" },
-        }),
-      })
-      -- Use buffer source for `/` and `?`
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-      -- Use cmdline & path source for ':'
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
+      -- LSPs
       lsp.pyright.setup({ capabilities = capabilities }) -- requires pyright to be installed
       lsp.gopls.setup({ capabilities = capabilities }) -- requires gopls to be installed
       lsp.tsserver.setup({ capabilities = capabilities }) -- requires typescript-language-server to be installed
