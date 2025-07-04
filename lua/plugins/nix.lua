d8ad8bcf180727ce45d8470a781ca14c3e43025a
Jose Storopoli <jose@storopoli.com>
2025-07-04 05:43:39 -0300

    feat(plugins): nix support with nixd

---
+++ lua/plugins/nix.lua
@@ -0,0 +1,44 @@
+-- Use nixd
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "nix" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nixd = {
+          mason = false,
+        },
+      },
+    },
+  },
+  {
+    "mrcjkb/telescope-manix",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("manix")
+    end,
+    keys = {
+      -- {
+      --   "<leader>sn",
+      --   ft = "nix",
+      --   function ()
+      --     require('telescope').extensions.manix.manix({cword = true})
+      --   end,
+      --   desc = "Nix Docs (Word)",
+      -- },
+      {
+        "<leader>sN",
+        ft = "nix",
+        "<cmd>Telescope manix<cr>",
+        desc = "Nix Docs",
+      },
+    },
+  },
+}
